887cbfeb265a80778a25b180938597a6facfa9f8
Desmond Andersen <75047245+desmondandersen@users.noreply.github.com>
2023-01-02 21:06:00 -0800

    Add camera, adjust light links

--- studioLighting.py
+++ studioLighting.py
@@ -7,6 +7,8 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(StudioLightingWidget, self).__init__(parent=parent)
 
+        self.backdrop = None
+        self.camera = None
         self.key_light = None
         self.fill_light = None
         self.back_light = None
@@ -24,8 +26,14 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
 
         # Add Lights Button
         self.btn_add_lights = QtWidgets.QPushButton('Add Lights')
-        self.btn_add_lights.clicked.connect(self.generate_lights)
-        main_layout.addWidget(self.btn_add_lights, 0, 1)
+        self.btn_add_lights.clicked.connect(self.add_lights)
+        main_layout.addWidget(self.btn_add_lights, 1, 0)
+
+        # Add Lights Button
+        self.btn_add_cam = QtWidgets.QPushButton('Add Camera')
+        self.btn_add_cam.clicked.connect(self.add_camera)
+        main_layout.addWidget(self.btn_add_cam, 2, 0)
+
 
         # Set main layout
         main_widget.setLayout(main_layout)
@@ -37,32 +45,60 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
             pm.delete('studioBackdrop')
 
         # Create mesh
-        pm.polyPlane(n='studioBackdrop', w=20, h=20, sw=2, sh=2)
+        self.backdrop = pm.polyPlane(n='studioBackdrop', w=20, h=20, sw=2, sh=2)
         pm.polyExtrudeEdge('studioBackdrop.e[0]', 'studioBackdrop.e[2]', kft=True, lt=(0, 5, 5))
         pm.polyExtrudeEdge('studioBackdrop.e[14]', 'studioBackdrop.e[17]', kft=True, lt=(0, 5, 5))
         pm.rotate('studioBackdrop', [0, 180, 0])
         pm.setAttr('studioBackdrop.displaySmoothMesh', 2)
 
         # Set pivot to center
-        pm.select('studioBackdrop')
-        pm.manipPivot(p=(0, 0, 0))
-        pm.delete(constructionHistory=True)
+        self.backdrop.manipPivot(p=(0, 0, 0))
+        self.backdrop.delete(constructionHistory=True)
         pm.select(clear=True)
 
-    def generate_lights(self):
+    def add_camera(self):
+        self.camera = pm.camera(position=(0, 4, 8), aspectRatio=0.66)
+
+    def add_lights(self):
         # Delete any existing lights
         if pm.objExists('keyLight'):
             pm.select('keyLight')
             pm.delete()
 
-        # Create keylight
+        if pm.objExists('fillLight'):
+            pm.select('fillLight')
+            pm.delete()
+
+        if pm.objExists('backLight'):
+            pm.select('backLight')
+            pm.delete()
+
+        # Create key light
         self.key_light = pm.shadingNode('areaLight', asLight=True)
         self.key_light.rename('keyLight')
         self.key_light.getTransform().translate.set(5, 5, 5)
+        self.key_light.getTransform().rotateX.set(-20)
+        self.key_light.getTransform().rotateY.set(45)
+
+        # Create fill light
+        self.fill_light = pm.shadingNode('areaLight', asLight=True)
+        self.fill_light.rename('fillLight')
+        self.fill_light.getTransform().translate.set(-5, 5, 5)
+        self.fill_light.getTransform().rotateX.set(-20)
+        self.fill_light.getTransform().rotateY.set(-45)
+
+        # Create back light
+        self.back_light = pm.shadingNode('areaLight', asLight=True)
+        self.back_light.rename('backLight')
+        self.back_light.getTransform().translate.set(-8, 1, -5)
+        self.back_light.getTransform().rotateX.set(160)
+        self.back_light.getTransform().rotateY.set(45)
+
+        # Break link to backdrop
+        if pm.objExists('backLight'):
+            pm.lightlink(b=True, light='backLight', object='studioBackdrop')
 
 
-
-def show_ui():
-    ui = StudioLightingWidget()  # Create instance of widget
-    ui.show(dockable=True, floating=True)
-    return ui
+ui = StudioLightingWidget()  # Create instance of widget
+ui.resize(500, 500)
+ui.show(dockable=True, floating=True)
