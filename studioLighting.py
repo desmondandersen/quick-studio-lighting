84a5dd9ce0bb1232e2c05dab20bbb61c940d226c
Desmond Andersen <desmond.andersen@devz.ai>
2023-01-04 00:11:13 -0800

    Refine UI

--- studioLighting.py
+++ studioLighting.py
@@ -15,7 +15,7 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
 
         self.setWindowTitle("Quick Studio")
 
-        # Main widget
+        # UI - Main widget
         self.main_widget = QtWidgets.QWidget()
         self.main_layout = QtWidgets.QGridLayout(self)
         self.main_widget.setLayout(self.main_layout)
@@ -36,6 +36,15 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         self.btn_add_cam.clicked.connect(self.add_camera)
         self.main_layout.addWidget(self.btn_add_cam, 2, 0)
 
+        # UI - Scroll Area
+        scroll_widget = QtWidgets.QWidget()
+        self.scroll_layout = QtWidgets.QVBoxLayout(scroll_widget)
+
+        scroll_area = QtWidgets.QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setWidget(scroll_widget)
+        self.main_layout.addWidget(scroll_area, 3, 0)
+
     def add_backdrop(self):
         # Delete any existing backdrop
         if pm.objExists('studioBackdrop'):
@@ -53,6 +62,8 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         pm.manipPivot(p=(0, 0, 0))
         pm.delete(constructionHistory=True)
         pm.select(clear=True)
+
+        self.add_backdrop_widget(backdrop=self.backdrop)
 
     def add_camera(self):
         self.camera = pm.camera(position=(0, 4, 8), aspectRatio=0.66)
@@ -97,12 +108,16 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
 
     def add_light_widget(self, light, name):
         widget = LightWidget(light, name)
-        self.main_layout.addWidget(widget)
+        self.scroll_layout.addWidget(widget)
+
+    def add_backdrop_widget(self, backdrop):
+        widget = BackdropWidget(backdrop)
+        self.scroll_layout.addWidget(widget)
 
 
-class LightWidget(QtWidgets.QWidget):
+class LightWidget(QtWidgets.QFrame):
     """
-    Light Widget: Control a specified light in the scene
+    Control a specified light in the scene
     """
     def __init__(self, light, name):
         super(LightWidget, self).__init__()
@@ -111,41 +126,60 @@ class LightWidget(QtWidgets.QWidget):
         self.isLinkedToBackdrop = True
 
         # UI
-        layout = QtWidgets.QGridLayout(self)
+        layout = QtWidgets.QVBoxLayout(self)
+        self.setStyleSheet("QFrame { border: 1px solid black; border-radius: 8px; }")
+
+        # UI - Color and Name
+        color_widget = QtWidgets.QWidget()
+        color_layout = QtWidgets.QHBoxLayout(color_widget)
+        color_widget.setStyleSheet("QLabel { border: none }")
 
         label = QtWidgets.QLabel(self.name)
         label.setStyleSheet("font-size: 16px")
-        layout.addWidget(label, 0, 0)
+        color_layout.addWidget(label)
+
+        self.btn_color = QtWidgets.QPushButton()
+        self.btn_color.clicked.connect(self.update_color)
+        self.set_btn_color()
+        color_layout.addWidget(self.btn_color)
 
         # UI - Intensity
         intensity_widget = QtWidgets.QWidget()
         intensity_layout = QtWidgets.QHBoxLayout(intensity_widget)
-        layout.addWidget(intensity_widget, 1, 0)
+        intensity_widget.setStyleSheet("QLabel { border: none }")
 
         label = QtWidgets.QLabel('Intensity')
-        intensity_layout.addWidget(label, 0)
+        intensity_layout.addWidget(label)
+
         slider_intensity = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         slider_intensity.setMinimum(1)
         slider_intensity.setMaximum(1000)
         slider_intensity.setValue(self.light.intensity.get())
         slider_intensity.valueChanged.connect(lambda val: self.light.intensity.set(val))
-        intensity_layout.addWidget(slider_intensity, 1)
+        intensity_layout.addWidget(slider_intensity)
 
         # UI - Toggle visibility
+        toggles_widget = QtWidgets.QWidget()
+        toggles_layout = QtWidgets.QVBoxLayout(toggles_widget)
+
         toggle_visibility = QtWidgets.QCheckBox('Visibility')
         toggle_visibility.setChecked(self.light.visibility.get())
         toggle_visibility.toggled.connect(lambda val: self.light.getTransform().visibility.set(val))
-        layout.addWidget(toggle_visibility, 2, 0)
+        toggles_layout.addWidget(toggle_visibility)
 
         # UI - Toggle illuminate backdrop
         toggle_link = QtWidgets.QCheckBox('Illuminate backdrop (in render)')
         toggle_link.setChecked(self.isLinkedToBackdrop)
-        toggle_link.toggled.connect(lambda val: self.link_to_backdrop(val))
-        layout.addWidget(toggle_link, 3, 0)
+        toggle_link.toggled.connect(lambda val: self.update_lightlink(val))
+        toggles_layout.addWidget(toggle_link)
 
-    def link_to_backdrop(self, val):
+        layout.addWidget(color_widget)
+        layout.addWidget(intensity_widget)
+        layout.addWidget(toggles_widget)
+
+    def update_lightlink(self, val):
         """
-        :param val: true to make link; false to break link
+        :param val: true to make lightlink; false to break lightlink
         :type val: bool
         """
         light_name = str(self.light.getTransform())
@@ -156,8 +190,56 @@ class LightWidget(QtWidgets.QWidget):
             else:
                 pm.lightlink(b=True, light=light_name, object='studioBackdrop')
 
+    def update_color(self):
+        light_color = self.light.color.get()
+        color = pm.colorEditor(rgbValue=light_color)
+        r, g, b, a = [float(c) for c in color.split()]
+        color = (r, g, b)
+        self.light.color.set(color)
+        self.set_btn_color(color)
+
+    def set_btn_color(self, color=None):
+        """
+        :param color: tuple with RGB values
+        :type color: tuple
+        """
+        if not color:
+            color = self.light.color.get()
+        if len(color) == 3:
+            r, g, b = [c * 255 for c in color]
+            self.btn_color.setStyleSheet('background-color: rgba({0}, {1}, {2}, 1.0)'.format(r, g, b))
+
+
+class BackdropWidget(QtWidgets.QFrame):
+    """
+    Control backdrop in the scene
+    """
+    def __init__(self, backdrop):
+        super(BackdropWidget, self).__init__()
+        self.backdrop = backdrop
+
+        # UI
+        layout = QtWidgets.QVBoxLayout(self)
+        self.setStyleSheet("QFrame { border: 1px solid black; border-radius: 8px; }")
+
+        # UI - Color and Name
+        color_widget = QtWidgets.QWidget()
+        color_layout = QtWidgets.QHBoxLayout(color_widget)
+        color_widget.setStyleSheet("QLabel { border: none }")
+
+        label = QtWidgets.QLabel('Backdrop')
+        label.setStyleSheet("font-size: 16px")
+        color_layout.addWidget(label)
+
+        # self.btn_color = QtWidgets.QPushButton()
+        # self.btn_color.clicked.connect(self.update_color)
+        # self.set_btn_color()
+        # color_layout.addWidget(self.btn_color)
+
+        layout.addWidget(color_widget)
+
 
 def show_ui():
     ui = StudioLightingWidget()  # Create instance of widget
-    ui.resize(500, 500)
+    ui.resize(300, 500)
     ui.show(dockable=True, floating=True)
