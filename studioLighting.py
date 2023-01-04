8c33f09c2f2c9bc7118decf892db0c409165a017
Desmond Andersen <desmond.andersen@devz.ai>
2023-01-03 23:13:32 -0800

    Add light widget

--- studioLighting.py
+++ studioLighting.py
@@ -13,31 +13,28 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         self.fill_light = None
         self.back_light = None
 
-        self.setWindowTitle("EZ Studio")
+        self.setWindowTitle("Quick Studio")
 
         # Main widget
-        main_widget = QtWidgets.QWidget()
-        main_layout = QtWidgets.QGridLayout(self)
+        self.main_widget = QtWidgets.QWidget()
+        self.main_layout = QtWidgets.QGridLayout(self)
+        self.main_widget.setLayout(self.main_layout)
+        self.setCentralWidget(self.main_widget)
 
         # Add Backdrop Button
         self.btn_add_backdrop = QtWidgets.QPushButton('Add Backdrop')
         self.btn_add_backdrop.clicked.connect(self.add_backdrop)
-        main_layout.addWidget(self.btn_add_backdrop, 0, 0)
+        self.main_layout.addWidget(self.btn_add_backdrop, 0, 0)
 
         # Add Lights Button
         self.btn_add_lights = QtWidgets.QPushButton('Add Lights')
         self.btn_add_lights.clicked.connect(self.add_lights)
-        main_layout.addWidget(self.btn_add_lights, 1, 0)
+        self.main_layout.addWidget(self.btn_add_lights, 1, 0)
 
         # Add Lights Button
         self.btn_add_cam = QtWidgets.QPushButton('Add Camera')
         self.btn_add_cam.clicked.connect(self.add_camera)
-        main_layout.addWidget(self.btn_add_cam, 2, 0)
-
-
-        # Set main layout
-        main_widget.setLayout(main_layout)
-        self.setCentralWidget(main_widget)
+        self.main_layout.addWidget(self.btn_add_cam, 2, 0)
 
     def add_backdrop(self):
         # Delete any existing backdrop
@@ -52,8 +49,9 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         pm.setAttr('studioBackdrop.displaySmoothMesh', 2)
 
         # Set pivot to center
-        self.backdrop.manipPivot(p=(0, 0, 0))
-        self.backdrop.delete(constructionHistory=True)
+        pm.select('studioBackdrop')
+        pm.manipPivot(p=(0, 0, 0))
+        pm.delete(constructionHistory=True)
         pm.select(clear=True)
 
     def add_camera(self):
@@ -79,6 +77,7 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         self.key_light.getTransform().translate.set(5, 5, 5)
         self.key_light.getTransform().rotateX.set(-20)
         self.key_light.getTransform().rotateY.set(45)
+        self.add_light_widget(light=self.key_light, name="Key Light")
 
         # Create fill light
         self.fill_light = pm.shadingNode('areaLight', asLight=True)
@@ -86,6 +85,7 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         self.fill_light.getTransform().translate.set(-5, 5, 5)
         self.fill_light.getTransform().rotateX.set(-20)
         self.fill_light.getTransform().rotateY.set(-45)
+        self.add_light_widget(light=self.fill_light, name="Fill Light")
 
         # Create back light
         self.back_light = pm.shadingNode('areaLight', asLight=True)
@@ -93,12 +93,71 @@ class StudioLightingWidget(MayaQWidgetDockableMixin, QtWidgets.QMainWindow):
         self.back_light.getTransform().translate.set(-8, 1, -5)
         self.back_light.getTransform().rotateX.set(160)
         self.back_light.getTransform().rotateY.set(45)
+        self.add_light_widget(light=self.back_light, name="Back Light")
 
-        # Break link to backdrop
-        if pm.objExists('backLight'):
-            pm.lightlink(b=True, light='backLight', object='studioBackdrop')
+    def add_light_widget(self, light, name):
+        widget = LightWidget(light, name)
+        self.main_layout.addWidget(widget)
 
 
-ui = StudioLightingWidget()  # Create instance of widget
-ui.resize(500, 500)
-ui.show(dockable=True, floating=True)
+class LightWidget(QtWidgets.QWidget):
+    """
+    Light Widget: Control a specified light in the scene
+    """
+    def __init__(self, light, name):
+        super(LightWidget, self).__init__()
+        self.light = light
+        self.name = str(name)
+        self.isLinkedToBackdrop = True
+
+        # UI
+        layout = QtWidgets.QGridLayout(self)
+
+        label = QtWidgets.QLabel(self.name)
+        label.setStyleSheet("font-size: 16px")
+        layout.addWidget(label, 0, 0)
+
+        # UI - Intensity
+        intensity_widget = QtWidgets.QWidget()
+        intensity_layout = QtWidgets.QHBoxLayout(intensity_widget)
+        layout.addWidget(intensity_widget, 1, 0)
+
+        label = QtWidgets.QLabel('Intensity')
+        intensity_layout.addWidget(label, 0)
+        slider_intensity = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        slider_intensity.setMinimum(1)
+        slider_intensity.setMaximum(1000)
+        slider_intensity.setValue(self.light.intensity.get())
+        slider_intensity.valueChanged.connect(lambda val: self.light.intensity.set(val))
+        intensity_layout.addWidget(slider_intensity, 1)
+
+        # UI - Toggle visibility
+        toggle_visibility = QtWidgets.QCheckBox('Visibility')
+        toggle_visibility.setChecked(self.light.visibility.get())
+        toggle_visibility.toggled.connect(lambda val: self.light.getTransform().visibility.set(val))
+        layout.addWidget(toggle_visibility, 2, 0)
+
+        # UI - Toggle illuminate backdrop
+        toggle_link = QtWidgets.QCheckBox('Illuminate backdrop (in render)')
+        toggle_link.setChecked(self.isLinkedToBackdrop)
+        toggle_link.toggled.connect(lambda val: self.link_to_backdrop(val))
+        layout.addWidget(toggle_link, 3, 0)
+
+    def link_to_backdrop(self, val):
+        """
+        :param val: true to make link; false to break link
+        :type val: bool
+        """
+        light_name = str(self.light.getTransform())
+
+        if pm.objExists('studioBackdrop'):
+            if val:
+                pm.lightlink(light=light_name, object='studioBackdrop')
+            else:
+                pm.lightlink(b=True, light=light_name, object='studioBackdrop')
+
+
+def show_ui():
+    ui = StudioLightingWidget()  # Create instance of widget
+    ui.resize(500, 500)
+    ui.show(dockable=True, floating=True)
